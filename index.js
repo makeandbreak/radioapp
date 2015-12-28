bc93701f69ab6320f858092caabcb26383808c12
Rolf Missing <rolf.missing@gmail.com>
2015-12-28 07:58:13 +0100

    Administrative changes

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+// Copyright 2015 makeandbreak (github.com user id) 
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the software, and to permit
+// persons to whom the Software is furnished to do so, subject to the 
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRENTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+// USE OR OTHER DEALINGS IN THE SOFTWARE.  
+
+
+// https can be used if certificates are created and related code is uncommented 
+
 var fs = require("fs");
 var express = require('express');
 var bodyParser = require('body-parser');
 var stations = require("./stations.json");
 var util = require('util');
 var exec = require('child_process').exec;
-var https = require('https'); 
+// for ssl: var https = require('https'); 
 
-require('ssl-root-cas').addFile('./file.crt');
+// for ssl: require('ssl-root-cas').addFile('./file.crt');
 
 var childclear;
 var childadd;
@@ -16,14 +40,14 @@ var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var options = {
-    key: fs.readFileSync('./file.pem'),
-    cert: fs.readFileSync('./file.crt')
+    // for ssl: key: fs.readFileSync('./file.pem'),
+    // for ssl: cert: fs.readFileSync('./file.crt')
 };
 
-//var serverPort = 443;
+// for ssl: var serverPort = 443;
 var serverPort = 8081;
 
-var server = https.createServer(options, app);
+// for ssl: var server = https.createServer(options, app);
 
 global.selectedID = 0;
 
@@ -128,7 +152,7 @@ app.get("/setStation/:id", function(req, res){
 
 
 var server = app.listen(serverPort, function () {
-// server.listen(serverPort, function() {
+// for ssl: server.listen(serverPort, function() {
 
     var host = server.address().address
     var port = server.address().port
